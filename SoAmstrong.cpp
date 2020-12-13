b8e0169ff9a291439f1749a7227531691e9df3e2
creepsit <creepsxt@gmail.com>
2020-12-14 01:31:35 +0700

    commit on master

---
+++ SoAmstrong.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int sochuso(int n) // Tính số chữ số
+{
+    int count = 0;
+    for (int i = 0;; i++)
+    {
+        if (n >= 1)
+        {
+            n /= 10;
+            count += 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+int TONGcacchuso(int n) //Tổng "(các chữ số)^(số chữ số)
+{
+    int k = sochuso(n);
+    int sum = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        int dem1 = n / pow(10, k - i);
+        sum += pow(dem1, k);
+        n = n - dem1 * pow(10, k - i);
+    }
+    return sum;
+}
+int main()
+{   
+    int a;
+    cout << "Nhap a: "; cin >> a;
+    cout << "Các số Amstrong nhỏ hơn " << a << " là: ";
+    for(int i=1; i<a; i++)
+     
+     if (TONGcacchuso(i) == i)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
